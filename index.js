e644582fed72cd4834b9cf9c5b48c62d75277ad6
Your Name <kfarasat95@gmail.com>
2024-03-31 23:44:28 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) computer will generate a random number - done
+// 2) user input for guessing number - done
+// 3) computer user input with computer generated number and show result - done
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "please guess a number between 1-10: ",
+    }
+]);
+if (answers.userguessednumber === randomnumber) {
+    console.log("congtraulation! you guess right number.");
+}
+else {
+    console.log("you guess wrong number.");
+}
